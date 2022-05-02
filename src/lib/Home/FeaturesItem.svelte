0a2e5ef70b886078b0f77a4c1f69ef8d15112ab1
babakfp <44144724+babakfp@users.noreply.github.com>
2022-05-02 09:23:35 -0700

    features

---
+++ src/lib/Home/FeaturesItem.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let title
+  export let description
+  export let bgColor
+</script>
+
+<div class="grid gap-3">
+  <div class="flex items-center gap-3">
+    <div class="w-10 h-10 flex items-center justify-center {bgColor} bg-opacity-10 rounded-full">
+      <slot />
+    </div>
+    <span class="text-sm">{title}</span>
+  </div>
+  <p class="text-sm leading-7 font-extralight text-gray-400">{description}</p>
+</div>
