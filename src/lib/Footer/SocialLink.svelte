5b7049fcd893aaffa32ff2f743660f2869510f18
babakfp <44144724+babakfp@users.noreply.github.com>
2022-08-04 08:32:13 -0700

    Footer social links styling

--- src/lib/Footer/SocialLink.svelte
+++ src/lib/Footer/SocialLink.svelte
@@ -2,6 +2,6 @@
 	export let href
 </script>
 
-<a class="btn-base w-10 h-10 bg-white shadow rounded-full fill-gray-500 hover:bg-gray-100" {href}>
+<a class="btn-base w-10 h-10 bg-white shadow rounded-full fill-gray-500 hover:bg-brand hover:fill-white" {href}>
 	<slot />
 </a>
